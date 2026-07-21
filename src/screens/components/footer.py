5b7e4a78f3f5700f4c8d2ec04e412ce9520d5c91
ut6809 <utsavp9955@gmail.com>
2026-07-21 10:03:44 +0530

    homescreen

---
+++ src/screens/components/footer.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+def footer_home():
+    
+    st.header("SMART ATTENDANCE SYSTEM ")
+
+    logo_url = "https://ibb.co/DfzyYSnn.png"
+    
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:white;"> Created with ❤️ by </p>  
+        <img src='{logo_url}' style='max-height:25px' />
+        </div>
+                
+                """, unsafe_allow_html=True)
+
+
+def footer_dashboard():
+    logo_url = "https://i.ibb.co/4r5X1FY/apnacollege.png"
+    
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:black;"> Created with ❤️ by </p>  
+        <img src='{logo_url}' style='max-height:25px' />
+        </div>
+                
+                """, unsafe_allow_html=True)
