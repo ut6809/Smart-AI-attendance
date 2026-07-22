ef2fa8101944d848b28b0b4d2932fbc05c80904f
ut6809 <utsavp9955@gmail.com>
2026-07-23 02:28:26 +0530

    Update header.py

--- src/screens/components/header.py
+++ src/screens/components/header.py
@@ -10,7 +10,7 @@ def header_home():
     st.markdown(f"""
         <div style="display:flex; flex-direction:column; align-items:center; justify-content:center; margin-bottom:30px; margin-top:30px">
              <img src='{logo_url}' style='height:100px;' />
-            <h1 style='text-align:center; color:#E0E3FF'>SNAP<br/>CLASS</h1>
+            <h1 style='text-align:center; color:#E0E3FF'>Smart AI Attendance</h1>
         </div>   
                 
                 """, unsafe_allow_html=True)
@@ -22,7 +22,7 @@ def header_dashboard():
     st.markdown(f"""
         <div style="display:flex; align-items:center; justify-content:center; gap:10px">
             <img src='{logo_url}' style='height:85px;' />
-            <h2 style='text-align:left; color:#5865F2'>SNAP<br/>CLASS</h1>
+            <h2 style='text-align:left; color:#5865F2'>Smart AI Attendance</h1>
         </div>   
                 
                 """, unsafe_allow_html=True)
