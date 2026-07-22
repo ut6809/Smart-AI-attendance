c5ad447706dceb0b26cf241b5afd0647031973f9
ut6809 <utsavp9955@gmail.com>
2026-07-23 02:25:25 +0530

    few changes

--- src/screens/components/header.py
+++ src/screens/components/header.py
@@ -22,7 +22,7 @@ def header_dashboard():
     st.markdown(f"""
         <div style="display:flex; align-items:center; justify-content:center; gap:10px">
             <img src='{logo_url}' style='height:85px;' />
-            <h2 style='text-align:left; color:#5865F2'>Smart AI Attendance</h1>
+                <h2 style='text-align:left; color:#5865F2'>SNAP<br/>CLASS</h2>
         </div>   
                 
                 """, unsafe_allow_html=True)
